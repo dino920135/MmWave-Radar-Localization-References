8a04b7a22ff198bf56b63871e8cf4c42669395e7
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2022-09-21 00:55:59 +0800

    Create GetLogseqMd.sh

---
+++ .github/workflows/GetLogseqMd.sh
@@ -0,0 +1,9 @@
+git show
+cd ../
+git clone https://github.com/dino920135/Notes
+echo "$PWD"
+
+cd Notes/pages
+echo "$PWD"
+
+cat MmWave-Radar-Localization-References.md > ../../MmWave-Radar-Localization-References/README.md
