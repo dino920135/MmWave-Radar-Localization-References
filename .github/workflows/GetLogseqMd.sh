bc9de5376e656d66ebd1a59f3943f50e242cc061
Tsai Syun <65594575+dino920135@users.noreply.github.com>
2022-09-21 01:16:15 +0800

    Update GetLogseqMd.sh

--- .github/workflows/GetLogseqMd.sh
+++ .github/workflows/GetLogseqMd.sh
@@ -7,3 +7,4 @@ cd Notes/pages
 echo "$PWD"
 
 cat MmWave-Radar-Localization-References.md > ../../MmWave-Radar-Localization-References/README.md
+sed -i 's/..\/assets/https:\/\/github.com\/dino920135\/Notes\/blob\/main\/assets/' ../../MmWave-Radar-Localization-References/README.md
